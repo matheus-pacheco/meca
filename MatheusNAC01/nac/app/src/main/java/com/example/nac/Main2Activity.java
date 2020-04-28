c9d496c385c12bfd17a913af4a2b33a2ad029c65
matheus-pacheco <matheus-pacheco@users.noreply.github.com>
2020-04-28 09:41:49 -0300

    nac

--- MatheusNAC01/nac/app/src/main/java/com/example/nac/Main2Activity.java
+++ MatheusNAC01/nac/app/src/main/java/com/example/nac/Main2Activity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebResourceError;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
